ef3f0c91a01dfa6882737d19a391841c5dec3c8f
Jesse Gardner <jesse@plasticmind.com>
2017-01-09 15:30:34 -0500

    Logo updates

--- index.php
+++ index.php
@@ -31,6 +31,96 @@
   ================================================== -->
   <div id="bootstrap" class="container">
   
+
+    <!-- Top Nav
+    ================================================== -->  
+
+    <div class="row">
+      <div class="col-sm-12">
+
+        <div class="bs-component">
+          <nav class="navbar navbar-default">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+
+                <a class="navbar-brand" href="#">Enrichmond.com</a>
+
+              </div>
+
+              <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                  <li class="active"><a href="#">About <span class="sr-only">(current)</span></a></li>
+                  <li><a href="#">Partners</a></li>
+                  <li><a href="#">Events</a></li>
+                  <li><a href="#">Volunteer</a></li>
+                  <li><a href="#">Contact</a></li>
+                  <li><a href="#">Donate</a></li>
+
+                </ul>
+                <form class="navbar-form navbar-right" role="search">
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                  </div>
+                  <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+
+              </div>
+            </div>
+          </nav>
+        </div>
+
+        <div class="bs-component">
+          <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Brand</a>
+              </div>
+
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+                <ul class="nav navbar-nav">
+                  <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                  <li><a href="#">Link</a></li>
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                      <li><a href="#">Action</a></li>
+                      <li><a href="#">Another action</a></li>
+                      <li><a href="#">Something else here</a></li>
+                      <li class="divider"></li>
+                      <li><a href="#">Separated link</a></li>
+                      <li class="divider"></li>
+                      <li><a href="#">One more separated link</a></li>
+                    </ul>
+                  </li>
+                </ul>
+                <form class="navbar-form navbar-left" role="search">
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                  </div>
+                  <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+                <ul class="nav navbar-nav navbar-right">
+                  <li><a href="#">Link</a></li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div><!-- /example -->
+      </div>
+    </div>
+
     <div class="row text-center" style="margin:3em auto;">
       <h1 style="font-size:4em;">Enrichmond.com</h1>
       <p class="lead" style="font-size:2em;">Bootstrap-themed Style Guide</p>
