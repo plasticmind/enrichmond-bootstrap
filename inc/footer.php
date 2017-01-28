df41f708c6e099702e607921327c976ae7e2ba82
Jesse Gardner <jesse@plasticmind.com>
2017-01-28 09:07:44 -0500

    Added footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,5 @@
+
+
         </div>
       </div>
 
@@ -6,9 +8,67 @@
     <div class="page-footer">
       <div class="container">
 
+        <div class="row">
+          <div class="footer">
 
-       <p>© <?php echo date('Y'); ?> Enrichmond.com</p>
+            <div class="col-sm-3">
+              <h3><a href="/contact">Contact Us</a></h3>
+            </div>
 
+
+            <div class="vcard col-sm-3">
+              <div class="adr">
+                <p class="street-address">100 North 17th Street</p>
+                <p><span class="locality">Richmond</span>, <span class="region">VA</span>
+                  <span class="postal-code">23219</span></p>
+              </div>
+            </div>
+
+            <div class="col-sm-3 contact">
+              <p>Tel <span class="tel">804-646-0954</span></p>
+              <p><a href="mailto:jsydnor@enrichmond.org" class="mail">jsydnor@enrichmond.org</a></p>
+            </div>
+
+            <div class="col-sm-3">
+                <ul class="nav navbar-nav navbar-social navbar-right">
+                      <li><a href="https://instagram.com/enrichmondfoundation/" class="ig" target="_blank">
+                          <span class="fa fa-instagram"></span>
+                      </a></li>
+                      <li><a href="hhttp://www.facebook.com/pages/Enrichmond-Foundation/380464383435?ref=search&sid=657465541.2902090620..1" class="fb" target="_blank">
+                        <span class="fa fa-facebook"></span>
+                      </a></li>
+                      <li><a href="http://www.twitter.com/enrichmond" class="tw" target="_blank">
+                          <span class="fa fa-twitter"></span>
+                      </a></li>
+                      <li><a href="http://www.enrichmond.org/feed/" class="rss">
+                        <span class="fa fa-rss"></span>
+                      </a></li>
+                </ul>
+            </div>
+
+
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="footer">
+
+            <div class="col-sm-3 copyright">
+              <p>© <?php echo date('Y'); ?> <a href="http://enrichmond.org">Enrichmond.org</a> - <a href="/privacy-statement/" title="Privacy Policy">Privacy</a></p>
+
+            </div>
+
+            <div class="col-sm-6 note">
+              <p class="small">As part of our bylaws, initiatives supported by our Foundation must align with the Department of Parks, Recreation, and Community Facilities.</p>
+            </div>
+
+            <div class="col-sm-3">
+              <p class="richmond-badge"><a href="http://www.richmondgov.com/"><img src="assets/img/badge-richmond.png" alt="Richmond" width="70" height="55"></a></p>
+            </div>
+
+
+          </div>
+        </div>
 
       </div>
     </div>
--- index.php
+++ index.php
@@ -13,6 +13,17 @@ include('inc/header.php');
       <div class="page-body">
         <div class="container">
 
+        <h2>Page Templates</h2>
+        <ul>
+          <li><a href="partners.php">Partner Listing</a></li>
+          <li><a href="partner.php">Individual Partner</a></li>
+          <li><a href="events.php">Event Listing</a></li>
+          <li><a href="event.php">Individual Event</a></li>
+          <li><a href="blog.php">Blog</a></li>
+          <li><a href="article.php">Individual Blog Post</a></li>
+          
+        </ul>
+
 
 <?php 
 
