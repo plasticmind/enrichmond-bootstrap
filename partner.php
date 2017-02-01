3e6ce7ea26f947d02ceda95b23635a4fb611fedd
Jesse Gardner <jesse@plasticmind.com>
2017-02-01 10:33:41 -0500

    Fixed map JS logic

--- partner.php
+++ partner.php
@@ -31,9 +31,9 @@ include('inc/header.php');
 
             <p><em>2016 operating dates for the market are April 20th through November 3rd.</em></p>          
           </div>
-          
+
           <div class="partner-map">
-                <div id="gmap-partner" class="gmap"></div>
+                <div id="gmap-partner" class="gmap" data-address="Richmond, VA"></div>
           </div>
         </div>
       </div>
@@ -41,30 +41,34 @@ include('inc/header.php');
 <?php 
 
 $footer_inject = <<<EOT
-  <script src="http://maps.google.com/maps/api/js"></script>
+  <script src="http://maps.googleapis.com/maps/api/js?libraries=geometry,places&ext=.js"></script>
   <script src="/assets/js/gmaps.js"></script>
 
   <script>
 
-  var map = new GMaps({
-        div: '#gmap-partner',
-        lat: 0, 
-        lng: 0
-  });
+  function initialize() {
+    var map = new GMaps({
+          div: '#gmap-partner',
+          lat: 0, 
+          lng: 0,
+          zoom: 0
+    });
 
-  GMaps.geocode({
-    address: "Richmond, VA",
-    callback: function(results, status) {
-      if (status == 'OK') {
-        var latlng = results[0].geometry.location;
-        map.fitBounds(results[0].geometry.viewport);
-        map.addMarker({
-          lat: latlng.lat(),
-          lng: latlng.lng()
-        });
+    GMaps.geocode({
+      address: $('#gmap-partner').attr('data-address'),
+      callback: function(results, status) {
+        if (status == 'OK') {
+          var latlng = results[0].geometry.location;
+          map.fitBounds(results[0].geometry.viewport);
+          map.addMarker({
+            lat: latlng.lat(),
+            lng: latlng.lng()
+          });
+        }
       }
-    }
-  });
+    });
+  }
+  google.maps.event.addDomListener(window, "load", initialize);
 
   </script>
 EOT;
